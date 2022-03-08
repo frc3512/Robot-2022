55819c36d0374fb16b5e93fea8ef8b919309091a
MattS03 <m_santana03@yahoo.com>
2022-03-07 18:45:28 -0800

    Add boolean to disable logging

--- src/main/include/subsystems/ControlledSubsystemBase.hpp
+++ src/main/include/subsystems/ControlledSubsystemBase.hpp
@@ -50,16 +50,19 @@ public:
      *                       unit.
      * @param outputLabels   Labels for outputs each consisting of its name and
      *                       unit.
+     * @param isTesting      Whether or not the robot should log info.
      */
     ControlledSubsystemBase(
         std::string_view controllerName,
         const std::array<ControllerLabel, States>& stateLabels,
         const std::array<ControllerLabel, Inputs>& inputLabels,
-        const std::array<ControllerLabel, Outputs>& outputLabels)
+        const std::array<ControllerLabel, Outputs>& outputLabels,
+        bool isTesting = false)
         : m_csvLogger{controllerName, stateLabels, inputLabels, outputLabels},
           m_ntLogger{controllerName, stateLabels, inputLabels, outputLabels},
           m_timingLogger{fmt::format("{} timing", controllerName),
-                         "Loop duration (ms)", "Scheduling period (ms)"} {
+                         "Loop duration (ms)", "Scheduling period (ms)"},
+          m_isTesting(isTesting) {
         m_entryThreadRunning = true;
         m_entryThread = std::thread{[=] { EntryThreadMain(); }};
     }
@@ -100,6 +103,11 @@ public:
      * Returns true if the control loop is enabled.
      */
     bool IsEnabled() const { return m_isEnabled; }
+
+    /**
+     * Returns true if the team needs logging information
+     */
+    bool IsTesting() const { return m_isTesting; }
 
     /**
      * Returns the most recent timestep.
@@ -181,39 +189,43 @@ private:
     units::second_t m_nowBegin = frc::CSVLogFile::GetStartTime();
     units::second_t m_dt = Constants::kControllerPeriod;
     bool m_isEnabled = false;
+    bool m_isTesting = false;
 
     wpi::ConcurrentQueue<LogEntry> m_entryQueue;
     std::atomic<bool> m_entryThreadRunning{false};
     std::thread m_entryThread;
 
     void EntryThreadMain() {
-        if (!frc::SetCurrentThreadPriority(true, kPrioLoggingThread)) {
-            throw std::runtime_error(
-                fmt::format("Setting logging thread RT priority to {} failed\n",
-                            kPrioLoggingThread));
-        }
-
-        // Loops until the thread is told to exit and all remaining queue items
-        // have been written to the logs
-        while (m_entryThreadRunning || !m_entryQueue.empty()) {
-            auto entry = m_entryQueue.pop();
-            if (!entry.valid) {
-                continue;
+        if (IsTesting()) {
+            if (!frc::SetCurrentThreadPriority(true, kPrioLoggingThread)) {
+                throw std::runtime_error(fmt::format(
+                    "Setting logging thread RT priority to {} failed\n",
+                    kPrioLoggingThread));
             }
 
-            m_csvLogger.Log(entry.nowBegin - frc::CSVLogFile::GetStartTime(),
-                            entry.r, entry.x, entry.u, entry.y);
+            // Loops until the thread is told to exit and all remaining queue
+            // items have been written to the logs
+            while (m_entryThreadRunning || !m_entryQueue.empty()) {
+                auto entry = m_entryQueue.pop();
+                if (!entry.valid) {
+                    continue;
+                }
 
-            if (frc::DriverStation::IsDisabled() ||
-                !frc::DriverStation::IsFMSAttached()) {
-                m_ntLogger.Log(entry.r, entry.x, entry.u, entry.y);
+                m_csvLogger.Log(
+                    entry.nowBegin - frc::CSVLogFile::GetStartTime(), entry.r,
+                    entry.x, entry.u, entry.y);
+
+                if (frc::DriverStation::IsDisabled() ||
+                    !frc::DriverStation::IsFMSAttached()) {
+                    m_ntLogger.Log(entry.r, entry.x, entry.u, entry.y);
+                }
+
+                m_timingLogger.Log(
+                    entry.nowBegin - frc::CSVLogFile::GetStartTime(),
+                    units::millisecond_t{entry.nowEnd - entry.nowBegin}.value(),
+                    units::millisecond_t{m_dt}.value());
+                m_lastTime = m_nowBegin;
             }
-
-            m_timingLogger.Log(
-                entry.nowBegin - frc::CSVLogFile::GetStartTime(),
-                units::millisecond_t{entry.nowEnd - entry.nowBegin}.value(),
-                units::millisecond_t{m_dt}.value());
-            m_lastTime = m_nowBegin;
         }
     }
 };
